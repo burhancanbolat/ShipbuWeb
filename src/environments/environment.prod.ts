7d01fe883a0bd59c52cb766c120d237475b5fd7a
Burhan <burhancanbolat99@gmail.com>
2024-01-04 20:12:42 +0300

    budget

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment =  {
-    baseApiUrl : "https://localhost:7224/api",
+    baseApiUrl : "https://94.73.160.106:8081/api",
     youtubeApiKey : "AIzaSyBmNSD-ZbtaSrM_uA56axxeuQ5j5f5O-ko"
 };
