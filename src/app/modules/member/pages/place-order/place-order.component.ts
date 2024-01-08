8dc9e94cabb88b08d296add3165ac6083dc2a4a5
Önay YALÇINER <onay.yalciner@hotmail.com>
2024-01-08 23:34:05 +0300

    font

--- src/app/modules/member/pages/place-order/place-order.component.ts
+++ src/app/modules/member/pages/place-order/place-order.component.ts
@@ -1,9 +1,11 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { DxFormComponent } from 'devextreme-angular';
 import notify from 'devextreme/ui/notify';
-import { isEmpty } from 'rxjs';
+import * as dialog from 'devextreme/ui/dialog';
+
 import { TransportOrderItemFeaturesService } from 'src/app/services/transport-order-item-features.service';
 import { UtilityService } from 'src/app/services/utility.service';
+import { TransportOrderItemContainerTypesService } from 'src/app/services/transport-order-item-container-types.service';
 
 @Component({
   selector: 'app-place-order',
@@ -16,6 +18,7 @@ export class PlaceOrderComponent implements OnInit {
   constructor(
     protected readonly utilityService: UtilityService,
     protected readonly transportOrderItemFeaturesService: TransportOrderItemFeaturesService,
+    protected readonly transportOrderItemContainerTypesService: TransportOrderItemContainerTypesService,
   ) {
     this.validateTransportValue = this.validateTransportValue.bind(this);
   }
@@ -36,17 +39,17 @@ export class PlaceOrderComponent implements OnInit {
     {
       id: 0,
       text: 'Koli',
-      icon: 'bi bi-box',
+      icon: 'si si-si-box',
     },
     {
       id: 1,
       text: 'Palet',
-      icon: 'bi bi-boxes',
+      icon: 'si si-si-pallet',
     },
     {
       id: 2,
       text: 'Konteyner',
-      icon: 'bi bi-stack',
+      icon: 'si si-si-container',
     },
   ];
   protected currentOrderItemType: any = this.orderItemTypeTabs[0];
@@ -58,7 +61,14 @@ export class PlaceOrderComponent implements OnInit {
     await this.transportOrderItemFeaturesService.store.load({ filter: ['enabled', '=', 'true'] }).then((result: any) => {
       this.features = result.data;
       this.utilityService.loadingPanelVisible = false;
+      this.loadFromStorage();
     });
+  }
+
+  private loadFromStorage() {
+    let items = sessionStorage.getItem('items');
+    if (items)
+      this.items = JSON.parse(items);
   }
 
   protected showFeatureDescriptionPopup(feature: any) {
@@ -103,27 +113,48 @@ export class PlaceOrderComponent implements OnInit {
   }
 
   protected add() {
-    if (!this.newOrderItem.image) {
-      notify({
-        message: 'Lütfen görsel yükleyiniz.',
-        duration: 3000,
-        type: 'error'
-      });
-    } else {
-      if (this.newOrderItemForm.instance.validate().isValid) {
-        this.newOrderItem.type = { ...this.currentOrderItemType };
-        this.items.push({ ...this.newOrderItem });
-        this.resetForm();
-        this.newOrderItemForm.instance.clear();
-      }
-      else {
+
+    if (this.newOrderItemForm.instance.validate().isValid) {
+      if (!this.newOrderItem.image) {
         notify({
-          message: 'Lütfen gerekli alanları doldurunuz',
+          message: 'Lütfen görsel yükleyiniz.',
           duration: 3000,
           type: 'error'
         });
       }
+      else {
+        this.newOrderItem.type = { ...this.currentOrderItemType };
+        this.items.push({ ...this.newOrderItem });
+        this.resetForm();
+        this.newOrderItemForm.instance.clear();
+        this.saveToStorage();
+      }
     }
+    else {
+      notify({
+        message: 'Lütfen gerekli alanları doldurunuz',
+        duration: 3000,
+        type: 'error'
+      });
+    }
+
+  }
+
+  protected onRowRemoved(e: any) {
+    this.saveToStorage();
+  }
+
+  protected clearItems() {
+    dialog.confirm("Sevkiyat listesi temizlenecektir. Devam etmek istiyor musunuz?", "UYARI!").then((r) => {
+      if (r) {
+        this.items = [];
+        this.saveToStorage();
+      }
+    })
+  }
+
+  private saveToStorage() {
+    sessionStorage.setItem('items', JSON.stringify(this.items));
   }
 
   private resetForm() {
@@ -133,7 +164,7 @@ export class PlaceOrderComponent implements OnInit {
       height: null,
       width: null,
       length: null,
-      contents: null,
+      contents: "",
       products: null,
       features: [],
       image: null
@@ -154,26 +185,25 @@ export class PlaceOrderComponent implements OnInit {
         result = (e.value > 0);
         break;
       case "width":
-        result = (e.value > 0) || this.currentOrderItemType.id == 2;
-        break;
       case "height":
-        result = (e.value > 0) || this.currentOrderItemType.id == 2;
-        break;
       case "length":
         result = (e.value > 0) || this.currentOrderItemType.id == 2;
         break;
       case "contents":
-        result = e.value?.length >= 0 || this.currentOrderItemType.id == 2;
+        result = e.value || this.currentOrderItemType.id == 2;
         break;
       case "products":
         result = (e.value > 0) || this.currentOrderItemType.id != 0;
+        break;
+      case "containerType":
+        result = e.value && this.currentOrderItemType.id == 2;
         break;
 
     }
     return result;
   }
-  attachmentChanged(e: any, feature: any) {
-    console.log(e, feature);
+
+  protected attachmentChanged(e: any, feature: any) {
     const reader = new FileReader();
     reader.onloadend = (event) => {
       feature.attachment = reader.result;
